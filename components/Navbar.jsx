70daddf9b16c450e99d12813c71f0abfa9698a41
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-04-20 18:26:58 +0200

    Add auth pages to the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,18 @@
 import Logo from "./common/Logo";
+import Link from "next/link"
 
 export default function Navbar() {
     return (
-        <div className="p-5 shadow-lg">
+        <div className="p-5 px-12 flex justify-between">
             <Logo />
+            <div>
+                <Link href="/auth/login" passHref>
+                    <button className="p-2.5 px-5 hover:bg-gray-300 transition-all hover:shadow-lg">Login</button>
+                </Link>
+                <Link href="/auth/register" passHref>
+                    <button className="p-2.5 px-5 hover:bg-gray-300 transition-all hover:shadow-lg border">Register</button>
+                </Link>
+            </div>
         </div>
     );
 }
