44486d38c5eec7313e2f939b8ce915a4d9816c8e
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-04-20 19:44:18 +0200

    Fix warnings

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,13 +4,14 @@ import Link from "next/link"
 export default function Navbar() {
     return (
         <div className="container py-5 flex justify-between">
-            <Logo />
+            <Logo/>
             <div>
-                <Link href="/auth/login" passHref>
+                <Link href={"/auth/login"} passHref>
                     <button className="p-2.5 px-5 hover:bg-gray-300 transition-all hover:shadow-lg">Login</button>
                 </Link>
-                <Link href="/auth/register" passHref>
-                    <button className="p-2.5 px-5 hover:bg-gray-300 transition-all hover:shadow-lg border">Register</button>
+                <Link href={"/auth/register"} passHref>
+                    <button className="p-2.5 px-5 hover:bg-gray-300 transition-all hover:shadow-lg border">Register
+                    </button>
                 </Link>
             </div>
         </div>
