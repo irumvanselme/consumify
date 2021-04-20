ccc4d57edb5c183bff8a97c4280b1b982799eda1
IRUMVA HABUMUGISHA Anselme <andesanselme@gmail.com>
2021-04-20 18:37:12 +0200

    Index page made for ready to go

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.scss";
 import Page from "../layouts/Page";
 
 export default function Home() {
-    return <Page title="Home page .. ">this is the home page</Page>;
+    return (
+        <Page title="Home page .. ">
+            <div className="container mx-auto">
+                Anselme
+            </div>
+        </Page>
+    );
 }
